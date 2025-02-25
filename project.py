bc300433e5a3bd8495476d7b1968147ef2561615
Bhavya Soni <rkumarbhavy@gmail.com>
2025-02-25 17:18:05 +0530

    Added a lot of projects

--- project.py
+++ project.py
@@ -7,13 +7,234 @@ projects = {
         },
         {
             "name": "Netflix Clone",
-            "desc": "Built a simple netflix front-end clone with figma or any tool of your choice",
+            "desc": "Build a simple netflix front-end clone with figma or any tool of your choice",
             "link": "https://youtu.be/Tgat3-prVv4?si=5BGIeaJTDT73ckPv"
         },
         {
             "name": "Interactive Weather App",
-            "desc": "Built a weather app that fetches live weather data from an API and displays it beautifully",
+            "desc": "Build a weather app that fetches live weather data from an API and displays it beautifully",
             "link": "https://youtu.be/WZNG8UomjSI?si=k-icilv6MGYwNU5k"
+        },
+    ],
+    'Backend Developer': [
+        {
+            "name": "User Authentication System",
+            "desc": "Build a secure system enabling user registration and login with hashed passwords and session management",
+            "link": "https://youtu.be/HdBR59YnXFk?si=8Rnb8eNd-O4mv8aW"
+        },
+        {
+            "name": "RESTful API for a Blog",
+            "desc": "Build a blog API with CRUD operations for posts, user authentication, and database integration",
+            "link": "https://youtu.be/b8ZUb_Okxro?si=X5NGgOuGOKJ_-qGP"
+        },
+        {
+            "name": "URL Shortener",
+            "desc": "Build a web application that converts long URLs into shortened versions and redirects users accordingly",
+            "link": "https://www.freecodecamp.org/news/python-tutorial-how-to-create-a-url-shortener-using-flask/"
+        },
+    ],
+    'Full Stack Developer': [
+        {
+            "name": "E-Commerce Platform (MERN Stack)",
+            "desc": "Develop a comprehensive e-commerce application featuring product listings, user authentication, shopping cart functionality, and payment integration",
+            "link": "https://youtu.be/CvCiNeLnZ00?si=yQ-UWV7x13uROdiw"
+        },
+        {
+            "name": "Social Media Application (Django + React)",
+            "desc": "Create a social media platform where users can register, create profiles, post updates, follow others, and engage through comments and likes",
+            "link": "https://youtu.be/sVTrC24QpSo?si=rACCMHTHs7Eooc58"
+        },
+        {
+            "name": "Blogging Platform (MongoDB, ReactJS, Nodejs)",
+            "desc": "Build a dynamic blogging website allowing multiple users to register, create, edit, and delete posts, with features like comments and categories",
+            "link": "https://www.youtube.com/watch?v=J7BGuuuvDDk&list=PLqm86YkewF6QbR7QwqYWcAbl70Zhv0JUE"
+        },
+    ],
+    'Mobile App Developer': [
+        {
+            "name": "Recipe App (React Native)",
+            "desc": "Develop a cross-platform mobile app that allows users to browse, search, and save their favorite recipes, complete with ingredients and cooking instructions",
+            "link": "https://youtu.be/cdnneQjsoT0?si=PfvR1pdDgO-F5wcl"
+        },
+        {
+            "name": "Fitness Exercise App (Flutter)",
+            "desc": "Create a fitness app that provides users with exercise routines, tracking capabilities, and progress monitoring",
+            "link": "https://www.youtube.com/watch?v=fub5BBgyxO0&list=PLzcRC7PA0xWTlppoitIMlaE_EIiyTw97h"
+        },
+        {
+            "name": "Weather Forecast App (Kotlin)",
+            "desc": "Build an Android app using Kotlin that fetches real-time weather data from an API and displays forecasts for various locations",
+            "link": "https://www.youtube.com/watch?v=Z3Krx7utyKs&list=PLam6bY5NszYPU3Bk39HuPnnVVTaLhWIL0"
+        },
+    ],
+    'DevOps Engineer': [
+        {
+            "name": "Continuous Integration/Continuous Deployment (CI/CD) Pipeline with Jenkins",
+            "desc": "Set up a CI/CD pipeline using Jenkins to automate the process of integrating code changes, running tests, and deploying applications",
+            "link": "https://youtu.be/GCH33XeL_9I?si=Y14Q7akADlVYfdCo"
+        },
+        {
+            "name": "Infrastructure as Code (IaC) with Terraform",
+            "desc": "Use Terraform to define and provision infrastructure in a cloud environment, enabling consistent and repeatable deployments",
+            "link": "https://youtu.be/VCayKl82Lt8?si=geMn0bNLOnob8Xtl"
+        },
+        {
+            "name": "Monitoring and Logging with Prometheus and Grafana",
+            "desc": "Implement a monitoring system using Prometheus to collect metrics and Grafana to visualize them, ensuring system reliability and performance",
+            "link": "https://youtu.be/ddZjhv66o_o?si=egAthoChacE983-l"
+        },
+    ],
+    'Data Engineer': [
+        {
+            "name": "End-to-End Data Pipeline (Apache Airflow)",
+            "desc": "Create a data pipeline using Apache Airflow to orchestrate Spark jobs for data transformation",
+            "link": "https://youtu.be/Y_vQyMljDsE?si=CMIaL3CFs5ZSBmKe"
+        },
+        {
+            "name": "Stock Market Real-Time Data Analysis Using Kafka",
+            "desc": "Develop a real-time data pipeline that ingests, processes, and analyzes live stock market data using Apache Kafka",
+            "link": "https://youtu.be/KerNf0NANMo?si=Uvh87IozKHt3Wj7N"
+        },
+        {
+            "name": "ETL Pipeline with AWS Redshift, S3, and Glue",
+            "desc": "Construct an ETL pipeline that extracts data from S3, transforms it using AWS Glue, and loads it into Amazon Redshift",
+            "link": "https://www.startdataengineering.com/post/data-engineering-project-for-beginners-batch-edition/"
+        },
+    ],
+    'Data Scientist': [
+        {
+            "name": "Housing Prices Prediction",
+            "desc": "Develop a model to predict house prices based on various features using regression techniques",
+            "link": "https://youtu.be/NQQ3DRdXAXE?si=HUQg4DOnCy6Kmzll"
+        },
+        {
+            "name": "Credit Card Fraud Detection",
+            "desc": "Create a system to identify fraudulent credit card transactions using machine learning algorithms",
+            "link": "https://youtu.be/M_Cu7r9gik4?si=Mzy9mwUQfMcMkkLz"
+        },
+        {
+            "name": "Fake News Detection",
+            "desc": "Build a classifier to distinguish between real and fake news articles using natural language processing",
+            "link": "https://youtu.be/U6ieiJAhXQ4?si=3Lo3t0QIrZbKR8wf"
+        },
+    ],
+    '': [
+        {
+            "name": "",
+            "desc": "",
+            "link": ""
+        },
+        {
+            "name": "",
+            "desc": "",
+            "link": ""
+        },
+        {
+            "name": "",
+            "desc": "",
+            "link": ""
+        },
+    ],
+    '': [
+        {
+            "name": "",
+            "desc": "",
+            "link": ""
+        },
+        {
+            "name": "",
+            "desc": "",
+            "link": ""
+        },
+        {
+            "name": "",
+            "desc": "",
+            "link": ""
+        },
+    ],
+    '': [
+        {
+            "name": "",
+            "desc": "",
+            "link": ""
+        },
+        {
+            "name": "",
+            "desc": "",
+            "link": ""
+        },
+        {
+            "name": "",
+            "desc": "",
+            "link": ""
+        },
+    ],
+    '': [
+        {
+            "name": "",
+            "desc": "",
+            "link": ""
+        },
+        {
+            "name": "",
+            "desc": "",
+            "link": ""
+        },
+        {
+            "name": "",
+            "desc": "",
+            "link": ""
+        },
+    ],
+    '': [
+        {
+            "name": "",
+            "desc": "",
+            "link": ""
+        },
+        {
+            "name": "",
+            "desc": "",
+            "link": ""
+        },
+        {
+            "name": "",
+            "desc": "",
+            "link": ""
+        },
+    ],
+    '': [
+        {
+            "name": "",
+            "desc": "",
+            "link": ""
+        },
+        {
+            "name": "",
+            "desc": "",
+            "link": ""
+        },
+        {
+            "name": "",
+            "desc": "",
+            "link": ""
+        },
+    ],
+    '': [
+        {
+            "name": "",
+            "desc": "",
+            "link": ""
+        },
+        {
+            "name": "",
+            "desc": "",
+            "link": ""
+        },
+        {
+            "name": "",
+            "desc": "",
+            "link": ""
         },
     ],
 }
